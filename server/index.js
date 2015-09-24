8cb141975fe354d54b2f6cb5dca17ee734dbc383
Istvan Kiesz <ikiesz@rapidminer.com>
2015-09-24 17:06:01 +0200

    much paths, such variables

--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
-import * as routes from './routes/index';
-
 import express from 'express';
 
-var app = express()
-  .use('/static/js', express.static(__dirname + '/../' + 'client'))
-  .use('/static/fonts', express.static(__dirname + '/../' + 'fonts'))
-  .use('/static/css', express.static(__dirname + '/../' + 'css'))
-  .set('view engine', 'jade')
-  .set('views', __dirname + '/../' + 'jade/page/');
+import * as routes from './routes/index';
 
-app.get('/', routes.landing.index);
-app.get('/styleguide', routes.styleguide.index);
+const URL_PATH = {
+  static: {
+    js: '/static/js',
+    css: '/static/css',
+    font: '/static/fonts'
+  },
+  root: '/',
+  styleguide: '/styleguide'
+};
+
+const DIST_PATH = __dirname + '/..';
+
+console.log('dist path: %s', DIST_PATH);
+console.log('avaiable URL paths: %j', URL_PATH);
+
+var app = express();
+
+app.locals.urlPath = URL_PATH;
+
+app.use(URL_PATH.static.js, express.static(DIST_PATH + '/client'))
+  .use(URL_PATH.static.font, express.static(DIST_PATH + '/fonts'))
+  .use(URL_PATH.static.css, express.static(DIST_PATH + '/css'))
+  .set('view engine', 'jade')
+  .set('views', DIST_PATH + '/jade/page/');
+
+app.get(URL_PATH.root, routes.landing.index);
+app.get(URL_PATH.styleguide, routes.styleguide.index);
 
 app.listen(3000);
